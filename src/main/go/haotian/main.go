f58933d9cbf29130d48883302eac49c7e0169051
Haotian_Xue <dududaddy.xue@gmail.com>
2024-08-08 21:53:06 +0800

    check boundary

--- src/main/go/haotian/main.go
+++ src/main/go/haotian/main.go
@@ -23,6 +23,10 @@ type WeatherStations struct {
 var defaultOffset int64 = 100 // read some more bytes for next line
 
 func main() {
+	start := time.Now()
+	defer func() {
+		fmt.Println("execution time: ", time.Since(start))
+	}()
 
 	// read file
 	file, err := os.Open("../../../../data/weather_stations.csv")
@@ -45,7 +49,6 @@ func main() {
 	// send start points to offset channel
 	go func() {
 		i := 0
-		fmt.Println("file Size: ", fileState.Size(), "chunk size: ", chunkSizeMB)
 		for i < int(fileState.Size()) {
 			offsetCh <- i
 			i += chunkSizeMB
@@ -72,10 +75,41 @@ func main() {
 	}()
 
 	// merge to a map
+	var wholeMap map[string]WeatherStations
 	for parsed := range parsedWS {
-		fmt.Println(parsed)
+		if wholeMap == nil {
+			wholeMap = parsed
+		} else {
+			for k, v := range parsed {
+				wholeValue, exist := wholeMap[k]
+				if !exist {
+					wholeMap[k] = v
+				} else {
+					if v.Min < wholeValue.Min {
+						wholeValue.Min = v.Min
+					}
+
+					if v.Max > wholeValue.Max {
+						wholeValue.Max = v.Max
+					}
+
+					wholeValue.Total += v.Total
+					wholeValue.Count += v.Count
+					wholeValue.Avg = wholeValue.Total / float64(wholeValue.Count)
+				}
+			}
+		}
 	}
-	// simpleProcess(file)
+
+	counts := 0
+
+	for _ = range wholeMap {
+		counts++
+	}
+
+	fmt.Println("total:", counts)
+
+	simpleProcess(file)
 
 }
 
@@ -87,7 +121,11 @@ func parseFileAtOffset(file *os.File, offset int64) map[string]WeatherStations {
 	if err != nil && err != io.EOF {
 		panic(fmt.Sprintf("read from offset: %v has error: %v", offset, err))
 	}
-	fmt.Println("read file length: ", n)
+
+	if n == 100 {
+		fmt.Println(string(buf))
+	}
+
 	if len(buf) == 0 {
 		return map[string]WeatherStations{}
 	}
@@ -95,9 +133,13 @@ func parseFileAtOffset(file *os.File, offset int64) map[string]WeatherStations {
 	// skip first line for none first chunk
 	if offset != 0 {
 		var ch string
-		for ch != "\n" {
-			start++
+		for start < int64(n) {
 			ch = string(buf[start])
+			if ch != "\n" {
+				start++
+			} else {
+				break
+			}
 		}
 		start++
 	}
@@ -114,57 +156,62 @@ func parseFileAtOffset(file *os.File, offset int64) map[string]WeatherStations {
 	for start < int64(n) {
 		if isCity {
 			// for loop to read chars to get a city name, stop when meet ';' and set value flag
-			ch := string(buf[start])
-			for ch != ";" {
-				sb.WriteString(ch)
+			for start < int64(n) {
+				if buf[start] == ';' {
+					cityName = sb.String()
+					sb.Reset()
+					isCity = false
+					start++
+					break
+				} else {
+					sb.WriteByte(buf[start])
+				}
 				start++
-				ch = string(buf[start])
 			}
-
-			isCity = false
-			cityName = sb.String()
-			sb.Reset()
-			start++
 		} else {
 			// for loop to read chars to get a temperature, stop when meet '\n'
-			ch := string(buf[start])
-			for ch != "\n" {
-				sb.WriteString(ch)
+			for start < int64(n) {
+				ch := buf[start]
+
+				if ch == '\n' {
+					// eol, need to calculate
+					tempString = sb.String()
+					tempFloat, err := strconv.ParseFloat(tempString, 64)
+					if err != nil {
+						panic(err)
+					}
+					// set city to map
+					city, ok := wsMap[cityName]
+					if !ok {
+						wsMap[cityName] = WeatherStations{
+							Min:   tempFloat,
+							Max:   tempFloat,
+							Total: tempFloat,
+							Count: 1,
+						}
+					} else {
+						if tempFloat > city.Max {
+							city.Max = tempFloat
+						} else if tempFloat < city.Min {
+							city.Min = tempFloat
+						}
+						city.Total += tempFloat
+						city.Count++
+						city.Avg = city.Total / float64(city.Count)
+					}
+					cityName = ""
+					tempString = ""
+					isCity = true
+					sb.Reset()
+					start++
+					break
+				} else {
+					sb.WriteByte(ch)
+				}
 				start++
-				ch = string(buf[start])
-			}
-			isCity = true
-			tempString = sb.String()
-			sb.Reset()
-			tempFloat, err := strconv.ParseFloat(tempString, 64)
-			if err != nil {
-				panic(err)
-			}
-			// set city to map
-			city, ok := wsMap[cityName]
-			if !ok {
-				wsMap[cityName] = WeatherStations{
-					Min:   tempFloat,
-					Max:   tempFloat,
-					Total: tempFloat,
-					Count: 1,
-				}
-			} else {
-				if tempFloat > city.Max {
-					city.Max = tempFloat
-				} else if tempFloat < city.Min {
-					city.Min = tempFloat
-				}
-				city.Total += tempFloat
-				city.Count++
-				city.Avg = city.Total / float64(city.Count)
 			}
 
-			cityName = ""
-			tempString = ""
-			start++
 		}
-
 	}
 
 	return wsMap
@@ -180,10 +227,13 @@ func simpleProcess(file *os.File) {
 
 	reader := bufio.NewReader(file)
 
+	counts := 0
+
 	for err == nil {
 		str, _, err = reader.ReadLine()
 		// fmt.Println(string(str))
 		if strings.Contains(string(str), ";") {
+			counts++
 			v := strings.Split(string(str), ";")
 			city, temp := v[0], v[1]
 			tempFloat, err := strconv.ParseFloat(temp, 64)
@@ -241,5 +291,5 @@ func simpleProcess(file *os.File) {
 		}
 	}
 
-	fmt.Println(fmt.Sprintf("content %v, used: %vs", sb.String(), time.Since(start).Seconds()))
+	fmt.Println(fmt.Sprintf("used: %v, counts: %v\n", time.Since(start).Seconds(), counts))
 }
